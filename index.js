82893bdefdad2cedf1cfb6fa2bca6b35110a9fc4
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-02-01 22:27:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const {google} = require('googleapis');
+const axios = require('axios');
+const express = require('express');
 
 // var data = {"installed":{"client_id":"292514726084-rrkktbloshqkns97tki5qsuju84oab3m.apps.googleusercontent.com","project_id":"gmail-api-412303","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost"]}};
 
-var data ={"installed":{"client_id":"292514726084-9058iaee5fpttgk21mik1p2mta44n3gf.apps.googleusercontent.com","project_id":"gmail-api-412303","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost:1729/"]}}
+var data = {"installed":{"client_id":"292514726084-9058iaee5fpttgk21mik1p2mta44n3gf.apps.googleusercontent.com","project_id":"gmail-api-412303","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost:1729"]}}
 
 const oauth2Client = new google.auth.OAuth2(
   data['installed']['client_id'],
@@ -24,3 +26,42 @@ const url = oauth2Client.generateAuthUrl({
 });
 
 console.log(url);
+
+
+// ____________________________________________________
+
+
+const app = express();
+
+async function api(token){
+    console.log('api');
+    const response = await axios.get(
+        'https://gmail.googleapis.com/gmail/v1/users/mohammedjavidhedu@gmail.com/profile',
+        {
+            headers: {
+                authorization : "Bearer "+token,
+            }
+        }
+    );
+    console.log(response.data);
+    
+}
+
+app.listen(1729, () => {
+    console.log("server running.. on 1729");
+});
+
+app.get('/', (req, res) => {
+    const code = req.query.code;
+
+    var access_token;
+
+    oauth2Client.getToken(code, function (err, tokens) {
+        access_token = tokens['access_token'];
+        
+        api(access_token);
+    });
+
+    res.send(code);
+})
+
