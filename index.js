1a68e8decf99f6f453890c1da39040c027a385a9
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-02-01 08:29:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,21 +12,15 @@ const oauth2Client = new google.auth.OAuth2(
 
 // generate a url that asks permissions for Blogger and Google Calendar scopes
 const scopes = [
-  'https://www.googleapis.com/auth/blogger',
-  'https://www.googleapis.com/auth/calendar'
+  'https://mail.google.com/',
+  'https://www.googleapis.com/auth/gmail.compose',
+  'https://www.googleapis.com/auth/gmail.modify',
+  'https://www.googleapis.com/auth/gmail.readonly'
 ];
 
 const url = oauth2Client.generateAuthUrl({
-  // 'online' (default) or 'offline' (gets refresh_token)
   access_type: 'offline',
-
-  // If you only need one scope you can pass it as a string
   scope: scopes
 });
 
 console.log(url);
-
-// This will provide an object with the access_token and refresh_token.
-// // Save these somewhere safe so they can be used at a later time.
-// const {tokens} = await oauth2Client.getToken(code);
-// oauth2Client.setCredentials(tokens);
